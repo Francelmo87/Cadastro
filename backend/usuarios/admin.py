3cfd7637a4b917882f2fc45ed09a8622c3aed3cf
Francelmo <francelmo87@hotmail.com>
2024-11-25 10:22:23 -0300

    Criado o painel adm da app usuarios

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Titular, Endereco, Dependente
+
+
+class DependenteInline(admin.TabularInline):
+    model = Dependente
+    extra = 0
+
+
+class EnderecoInline(admin.TabularInline):
+    model = Endereco
+    extra = 0
+
+
+@admin.register(Titular)
+class UsuarioAdmin(admin.ModelAdmin):
+    inlines = (DependenteInline, EnderecoInline,)
+    list_display = ('nome', 'rg', 'cpf', 'nis', 'telefone', 'ativo',)
