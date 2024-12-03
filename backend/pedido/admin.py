5c4e67d842e009df0b5b07e47ca93c7e58231503
Francelmo <francelmo87@hotmail.com>
2024-12-03 10:46:01 -0300

    inserido na área administrativa da lista total de pedidos

--- backend/pedido/admin.py
+++ backend/pedido/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Pedido, PedidoVisitar, PedidoAbastecer
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+    search_fields = ('codigo',)
+    list_filter = ('status',)
+    date_hierarchy = 'created'
