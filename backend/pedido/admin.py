b96a9b9cf64d1bde39844853298e20f777167699
Francelmo <francelmo87@hotmail.com>
2025-01-07 22:48:00 -0300

    quase no final

--- backend/pedido/admin.py
+++ backend/pedido/admin.py
@@ -4,8 +4,25 @@ from django.contrib import admin
 
 from .models import Pedido, PedidoVisitar, PedidoAbastecer
 
+
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+    search_fields = ('codigo',)
+    list_filter = ('status',)
+    date_hierarchy = 'created'
+
+
+@admin.register(PedidoVisitar)
+class PedidoVisitarAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+    search_fields = ('codigo',)
+    list_filter = ('status',)
+    date_hierarchy = 'created'
+
+
+@admin.register(PedidoAbastecer)
+class PedidoAbastacerAdmin(admin.ModelAdmin):
     list_display = ('__str__',)
     search_fields = ('codigo',)
     list_filter = ('status',)
