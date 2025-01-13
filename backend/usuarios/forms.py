9ec9d887cdeeb901949355dbd14a6efd9c9c57fa
Francelmo <francelmo87@hotmail.com>
2025-01-12 22:25:25 -0300

    Colocada o widget de calendario em data da validade

--- backend/usuarios/forms.py
+++ backend/usuarios/forms.py
@@ -3,6 +3,17 @@ from .models import Titular
 
 
 class UsuarioForm(forms.ModelForm):
+    dt_validade = forms.DateField(
+        label='Data da Validade',
+        required=False,
+        widget=forms.DateInput(
+            format='%Y-%m-%d',
+            attrs={
+                'type': 'date'
+            }),
+        input_formats=('%Y-%m-%d',),
+    )
+
     class Meta:
         model = Titular
         fields = '__all__'
